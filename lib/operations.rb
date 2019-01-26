71ac91a0dc582b20b878e04984f4ebc0005c679a
github username <github email address>
2019-01-25 19:22:24 -0500

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
 
+	if speed < 60 && speed > 40
+		false
+	elsif speed > 60
+		true
+	else speed < 40
+		true
+	end
+
 end
 
 
 
 def not_safe?(speed)
-	
+
+	speed > 60 || speed < 40 ? true : false
+
 end
-	
-
-
